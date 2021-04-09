670800de10ca795835fd74342eb401b0e3b0cd5f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-04-09 11:41:32 +0200

    Add synopsis to run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env sh
 
+# Synopsis:
+# Run the test runner on a solution.
+
+# Arguments:
+# $1: exercise slug
+# $2: absolute path to solution folder
+# $3: absolute path to output directory
+
+# Output:
+# Writes the test results to a results.json file in the passed-in output directory.
+# The test results are formatted according to the specifications at https://github.com/exercism/docs/blob/main/building/tooling/test-runners/interface.md
+
+# Example:
+# ./bin/run.sh two-fer /absolute/path/to/two-fer/solution/folder/ /absolute/path/to/output/directory/
+
+# If any required arguments is missing, print the usage and exit
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "usage: ./bin/run.sh exercise-slug /absolute/path/to/two-fer/solution/folder/ /absolute/path/to/output/directory/"
+    exit 1
+fi
+
 scheme --script "bin/exercise.ss" $@
